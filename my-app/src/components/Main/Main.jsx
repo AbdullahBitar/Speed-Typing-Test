f69e0fcbd5c9d384d31ab413b2e21ade575ce8e3
Abdullah Bitar <a@abdullahs-macbook-air.home>
2024-02-09 06:43:21 +0300

    added random words

--- my-app/src/components/Main/Main.jsx
+++ my-app/src/components/Main/Main.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './main.css';
 
 const Main = () => {
-    return (
-      <div>
-        <h1>Welcome to Speedy Speedy</h1>
-        <p class="instruc">You have 60 seconds. Once you press a button the timer will start. type as fast as you can!</p>
-        <div class ="out">
-          <p class="rec"></p>
-          <input class = "txt" type="text"></input>
-        </div>
-        
+  const allWords = ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us', 'very', 'many', 'much', 'before', 'those', 'too', 'may', 'look', 'more', 'where', 'such'];
+  const [all, setAll] = useState("");
+
+  useEffect(() => {
+    const n = 100;
+    let allText = "";
+    for (let i = 0; i < n; i++) {
+      const idx = Math.floor(Math.random() * allWords.length);
+      allText += allWords[idx] + " ";
+    }
+    setAll(allText);
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to Speedy Speedy</h1>
+      <p className="instruc">You have 60 seconds. Once you press a button the timer will start. type as fast as you can!</p>
+      <div className="out">
+        <p className="rec">{all}</p>
+        <input className="txt" type="text" />
       </div>
-    )
-  }
-  
-  export default Main
+    </div>
+  );
+};
+
+export default Main;
